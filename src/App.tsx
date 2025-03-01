43775baffdfa329aea6e893dd8176cd0581ed676
diegotero <diegotero.cba@gmail.com>
2025-03-01 08:45:32 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import ProductLines from './components/ProductLines';
 import WhatWeDoSection from './components/WhatWeDoSection';
 import { trackEvent } from './components/Analytics';
 
-// Image URLs
+// Image URL
 const HERO_MOBILE_URL = "https://vrrohcaovubbp0uw.public.blob.vercel-storage.com/clientes/spazio/bg-hero-aberturas-spazio-y63FuqTWk6UYy6elWeJWYcxRGohWWd.avif";
 const HERO_DESKTOP_URL = "https://vrrohcaovubbp0uw.public.blob.vercel-storage.com/clientes/spazio/bg-hero-aberturas-spazio-y63FuqTWk6UYy6elWeJWYcxRGohWWd.avif";
 
